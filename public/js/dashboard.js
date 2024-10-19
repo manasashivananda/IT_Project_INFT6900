5fa8052d0b87a15dae4cd5c5374544ae288c6e94
Mansi <mansi@mansis-air.newcastle.edu.au>
2024-10-19 13:50:22 +1100

    dashboard logic

---
+++ public/js/dashboard.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+  $("#refreshStats").click(function () {
+    // Simulating an AJAX call to refresh stats
+    alert("Refreshing statistics...");
+    // You can add an actual AJAX call here to fetch new statistics
+  });
+});
